8a569b5a8e828cef4a2aa1163829abf4d7dcd138
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-27 10:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

---
+++ chapter3/array_declaration.js
@@ -0,0 +1,2 @@
+let list_of_numbers = [2, 3, 5, 7, 11];
+console.log("First Number " + list_of_numbers[1]);
---
+++ chapter4/array_declaration.js
@@ -0,0 +1,9 @@
+let list_of_numbers = [2, 3, 5, 7, 11];
+console.log("First Number " + list_of_numbers[0]);
+console.log("Third Number " + list_of_numbers[2]);
+
+console.log("length of list of number is " + list_of_numbers["length"]);
+console.log("length of list of number using dot notation is " + list_of_numbers.length);
+
+let doh = "Doh";
+console.log("value of doh in upper case is " + doh.toUpperCase());

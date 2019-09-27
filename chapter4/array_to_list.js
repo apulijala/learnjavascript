e925d6dda62e527b4518d056ab86cbd8622eb0c7
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-27 22:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter4/array_to_list.js
+++ chapter4/array_to_list.js
@@ -19,39 +19,28 @@ function array_to_list(myArray) {
 }
 
 var result = array_to_list([1]);
-console.log(result);
-
 result1 = array_to_list([1, 2, 3]);
 console.log(result1);
+
 
 function list_to_array(myObject) {
 
     function list_to_array_helper(arrayToReturn, objToExtract) {
-        if (myObject.rest == null) {
 
-            console.log("Array to return");
+        if (objToExtract.rest == null) {
+
             arrayToReturn.push(objToExtract.value);
-            console.log(arrayToReturn);
             return arrayToReturn;
-        }else {
-            
-            arrayToReturn.push(objToExtract.value);
-            console.log("Array to return is ");
-            console.log(arrayToReturn);
+        } else {
 
-            return list_to_array(myObject.rest)
+            arrayToReturn.push(objToExtract.value);
+            return  list_to_array_helper(arrayToReturn, objToExtract.rest );
         }
     }
 
-   return list_to_array([], myObject);
+   return list_to_array_helper([], myObject);
 }
 
-var myArray1 = list_to_array(result);
-console.log("My array 1 is " );
-console.log(myArray1);
-
-/*
 var myArray2 = list_to_array(result1);
-console.log("My array 1 is " );
-console.log(myArray1);
-*/
+console.log("Array returned is ")
+console.log(myArray2);

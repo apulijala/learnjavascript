3e724e1b68d7159492466b26d8e5d8ccffd71825
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-27 11:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter4/obj-declaration.js
+++ chapter4/obj-declaration.js
@@ -14,10 +14,25 @@ let descriptions = {
      work : "Went to Work",
     "touched tree" : "Touched a Tree"
 };
-console.log("description of work is " + descriptions["work"]);
+console.log("description of work is " + descriptions.work);
 console.log("touched tree is " + descriptions["touched tree"]);
 // delete day1["wolf"];
 console.log("Wolf is " + day1.wolf)
+
+let description = {
+
+    "left" : 1, 
+    "right" : 2
+};
+
+console.log(description.left + " "  + description.right);
+console.log(" left is " + description["left"] + " and right is  "  + description["right"]);
+var leftInDescription = "left" in description; 
+console.log( "Is left in description ? " + leftInDescription);
+delete description["right"];
+var rightDescription = "right" in description;
+console.log( "Is right in description ? " + rightDescription);
+
 
 
 

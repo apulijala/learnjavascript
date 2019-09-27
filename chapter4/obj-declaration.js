3c3770fd18f1523985202f6715386aeccfb55c3e
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-27 11:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter4/obj-declaration.js
+++ chapter4/obj-declaration.js
@@ -4,3 +4,20 @@ var day1 = {
 };
 
 console.log(" Squirrel ? " + day1.squirrel);
+console.log(" Wolf ? " + day1.wolf);
+day1.wolf = true;
+console.log("  Wolf ? " + day1.wolf);
+console.log("event one : " + day1.events[0]);
+
+let descriptions = {
+
+     work : "Went to Work",
+    "touched tree" : "Touched a Tree"
+};
+console.log("description of work is " + descriptions["work"]);
+console.log("touched tree is " + descriptions["touched tree"]);
+// delete day1["wolf"];
+console.log("Wolf is " + day1.wolf)
+
+
+

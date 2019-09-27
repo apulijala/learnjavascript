ca6e3716a4fc15beb0111b2abce476bd02fce964
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-27 18:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter4/range.js
+++ chapter4/range.js
@@ -9,15 +9,41 @@ function range(low, high, step) {
     return numbers;
 }
 
+function reverseArrayInPlace(arrToReverse) {
+    for (var i = 0; i < arrToReverse;i++) {
+
+    }
+    return arrToReverse;
+}
+
+var originalArray = ["hello", "world", "enough"];
+console.log("Original Array is " );
+console.log(originalArray);
+var reversedArray = reverseArrayInPlace(originalArray);
+console.log("Reversed array is ");
+console.log(reversedArray);
+
+originalArray = ["hello", "world", "enough", "there"];
+console.log("Original Array is " );
+console.log(originalArray);
+reversedArray = reverseArrayInPlace();
+console.log("Reversed array is ");
+console.log(reversedArray);
+
+
+
 // numbers must be an array.
 function sum(numbers) {
 
+    console.log(typeof(numbers));
+    if (typeof(numbers) != "object") {
+        return numbers;
+    }
 
     let sum =  0;
     for (var k = 0; k < numbers.length; k++) {
         sum += numbers[k];
     }
-
     return sum;
 }
 

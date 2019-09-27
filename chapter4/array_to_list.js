214891669ab4cdc89aaa412e2fdf1fe7028b3320
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-27 21:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter4/array_to_list.js
+++ chapter4/array_to_list.js
@@ -1,7 +1,34 @@
 
 function array_to_list(myArray) {
 
-    unshift(myArray);
+    if (myArray.length == 1) {
+
+        var lead = myArray.shift();
+        var myObject= {};
+        myObject.value = lead;
+        myObject.rest = null;
+        return myObject;
+
+    }else {
+        
+        var myObject = {};
+        myObject.value = myArray.shift();
+        myObject.rest = array_to_list(myArray);
+        return myObject;
+    }
 }
 
-array_to_list([1, 2, 3]);
+var result = array_to_list([1]);
+console.log(result);
+
+result = array_to_list([1, 2, 3]);
+console.log(result);
+
+function list_to_array(myObject) {
+    if (myObject.rest == null) {
+        myArray = [];
+        return myArray.push();
+    }
+}
+
+list_to_array(result);

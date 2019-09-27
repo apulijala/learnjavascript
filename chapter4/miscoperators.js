d3392ea283ec8b526538eff38d80a33ea96070b4
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-27 16:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

---
+++ chapter4/miscoperators.js
@@ -0,0 +1,18 @@
+var numArray = [0, 1, 2, 3, 4];
+
+// Get elements from 2 onwards
+console.log(numArray);
+console.log(numArray.splice(2));
+console.log(numArray);
+console.log(numArray.splice(1,3));
+
+let tasks = ["Swimming", "Jogging", "Running", "Marathon"];
+console.log("Before adding Horse racing");
+console.log(tasks);
+tasks.unshift("Horse Racing");
+console.log("After adding Horse racing");
+console.log(tasks);
+console.log("After removing Horse Racing");
+var taskRemoved = tasks.shift();
+console.log(tasks);
+console.log("Task removeds is " + taskRemoved);

41f1624a3273b0d22eee9a48f32719c036419608
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-27 17:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter4/stringprops.js
+++ chapter4/stringprops.js
@@ -4,3 +4,25 @@ var stringToSplit = "Secretarybirds specialize in stomping";
 let strArray = stringToSplit.split(" ");
 console.log(strArray);
 console.log(strArray.join(". "));
+console.log("LA".repeat(3));
+console.log(String(6).padStart(3,"0"));
+
+function max(...args) {
+    let result = -Infinity;
+    for (let number of args) {
+        if (number > result) {
+            result = number;
+        }
+        
+    }
+    return result;
+}
+
+var result = max(4, 1, 9, -2);
+var newNumbers = [4, 1, 9, -2];
+console.log("Maximum value is " + result);
+
+max(...newNumbers);
+
+let {name} = {name: "Faraji", age: 23};
+console.log(name);

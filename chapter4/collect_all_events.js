f8f498d71021edb826db4000b3c8dc90e06b7801
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-27 15:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter4/collect_all_events.js
+++ chapter4/collect_all_events.js
@@ -1,6 +1,5 @@
 let journal = [];
 function addEntry(events, squirrel) {
-
     journal.push({events, squirrel});
 }
 
@@ -8,15 +7,17 @@ addEntry(["work", "touched tree", "pizza", "running", "television"], false);
 addEntry(["work", "ice cream", "cauliflower", "lasagna", "touched tree", "brushed teeth"], false);
 addEntry(["weekend", "cycling", "break", "peanuts",  "beer"], true);
 
-
-
 function collect_all_evetns() {
     let events = [];
     for (var i = 0; i < journal.length; i++) {
-        console.log(" outer i is " + i) ;
-        for (var i =0; i < journal.events.length; i++) {
-                console.log(" i is " + i);
-                console.log(journal.events[i]);
+        for (var j = 0; j < journal[i].events.length; j++) {
+
+            if (!events.includes(journal[i][j])  ) {
+
+            }
+            console.log(" events are " + journal[i].events[j]);
         }
     }
+
 }
+collect_all_evetns();

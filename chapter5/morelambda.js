71649f8506601d8dd7e439df035044871f2fb60e
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-28 19:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter5/morelambda.js
+++ chapter5/morelambda.js
@@ -6,3 +6,14 @@ function unless(test, then) {
 
 
 ['A', 'B'].forEach(l => console.log(l));
+
+
+function filter(array, test) {
+    let passed = [];
+    for (script of array) {
+            if (test(elem)) {
+                    passed.push(script);
+            }
+    }
+    return passed;
+}

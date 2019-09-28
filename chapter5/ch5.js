0f4d93629efbd068fce2d432348b99e0ef97f235
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-28 16:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter5/ch5.js
+++ chapter5/ch5.js
@@ -6,10 +6,39 @@ function repeat(n, action) {
 }
 
 repeat(5, console.log);
+var labels = [];
+repeat(5, k => {
 
-let labels = [];
-repeat(5, j => {
-    labels.push(`Unit $(1 + j)`);
-
+    labels.push(`Unit ${k + 1}`);
 });
 console.log(labels);
+
+// define an inner function and return it. 
+
+function greaterThan(n) {
+    function testGreaterThan(m) {
+        return m > n;
+    }
+    return testGreaterThan;
+}
+
+let greaterThan10 = greaterThan(10);
+console.log("Is 11 greater than 11 ? ");
+var greaterThan = greaterThan10(11);
+console.log(greaterThan);
+
+function greterThanWithExpression(n) {
+    return m => m > n;
+}
+
+greaterThan10 = greterThanWithExpression(10);
+console.log("Is 11 greater than 11 ? ");
+greaterThan = greaterThan10(11);
+console.log(greaterThan);
+
+
+
+
+
+
+

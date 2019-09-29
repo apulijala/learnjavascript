b5e08129eff500df75e4e3982d31ca84e3bd1dd0
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-29 18:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter5/exercises.js
+++ chapter5/exercises.js
@@ -1,5 +1,26 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
-let combined = [];
-arrays.map((arg) => {  
-    combined
-});
+
+function flatten(arrays) {
+    let combined = [];
+    arrays.map((arg) => {  
+    arg.forEach(i => combined.push(i));
+    });
+    return combined;
+}
+console.log(flatten(arrays));
+
+
+function flattenWithConcat(arrays) {
+
+    let combined = [];
+    arrays.map(arg =>  {
+        combined = combined.concat(arg );
+        
+    });
+    return combined;
+}
+console.log(flattenWithConcat(arrays));
+
+console.log("In reduction");
+var finalresult = arrays.reduce((flat, current ) => flat.concat(current), []);
+console.log(finalresult);

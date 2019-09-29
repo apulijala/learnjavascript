987e5ad617b1c17578fb0eb45829c969a02e3a90
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-29 11:30:02 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter5/reduction.js
+++ chapter5/reduction.js
@@ -14,3 +14,6 @@ console.log(reduce([1,2,3,4,5],
 console.log(
     "array reduced is " + 
     [1,2,3,4].reduce((a,b) => a + b));
+    
+    var ranges = [[65, 91], [75,100], [85,120],[90,130]];
+    

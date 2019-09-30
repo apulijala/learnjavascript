009d23ecc71bdd0145d83220def12d59b1b357aa
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-30 14:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter6/demomaps.js
+++ chapter6/demomaps.js
@@ -1,18 +1,28 @@
 let ages = {
-
-"Boris" : 39,
-"Liang" : 22,
-"Julia" : 62
+    "Boris" : 39,
+    "Liang" : 22,
+    "Julia" : 62
 };
 
-console.log("Júlia is " + ages["Julia"]);
-console.log("Is Jack's age known? " ,  "Jack" in ages);
+console.log("Prototype of ages " , Object.getPrototypeOf(ages));
+console.log("Julia is ", ages["Julia"]);
+console.log("Is Jack's age known?", "Jack" in ages);
+console.log("Is toString's age known?", "toString" in ages);
+
 
 ages = new Map();
-ages.set("Boris", 39);
-ages.set("Liang", 22);
-ages.set("Julia", 62);
+ages.set("Boris",39);
+ages.set("Liang",22);
+ages.set("Julia",62);
 
+console.log("Prototype of Map " , Object.getPrototypeOf(ages));
 console.log("Julia is ", ages.get("Julia"));
 console.log("Is Jack's age known?", ages.has("Jack"));
-console.log("Is Jack's age known?", ages.has("Jack"));
+console.log("Is Julia's age known?", ages.has("Julia"));
+console.log("Is toString's age known?", ages.has("toString"));
+
+
+
+
+
+
---
+++ setjavaenv.js
@@ -0,0 +1,2 @@
+export PATH=$PATH:/home/arvind/nodesoftware/node-v10.16.3-linux-x64/bin
+

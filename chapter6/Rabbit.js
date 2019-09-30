f687dd3823115a006c4737c11c89ddf2ab05dde5
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-30 12:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter6/Rabbit.js
+++ chapter6/Rabbit.js
@@ -3,9 +3,18 @@ function Rabbit(type) {
 }
 
 Rabbit.prototype.speak = function(line) {
-    console.log(`The ${this.type} Rabbit says '${line}' `);
+    console.log(`The ${this.type} says '${line}'`);
 }
-Rabbit.prototype.hello = "world";
 
-var weiredRabbit = new Rabbit("weird");
-weiredRabbit.speak("weird");
+var weiredRabbit = 
+    new Rabbit("weired");
+weiredRabbit.speak("weired!");
+
+console.log("Rabbit's prototype is ");
+console.log(Object.getPrototypeOf(Rabbit));
+console.log(Rabbit.prototype);
+
+
+console.log("Weired Rabbit's prototype is ");
+console.log(Object.getPrototypeOf(weiredRabbit));
+console.log(weiredRabbit.prototype);

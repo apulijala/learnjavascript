d7585b36af1f3e3e592429aee7902257b6ff1587
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-30 09:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter6/encapsulation.js
+++ chapter6/encapsulation.js
@@ -1,6 +1,15 @@
 let rabbit = {};
 rabbit.speak = function(line) {
-    console.log("Rabbit says ${line}");
+    console.log(`Rabbit says ${line}`);
 }
+rabbit.speak("Rama's Glory!");
 
-rabbit.speak("Jaya Guru Datta");
+function speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+}
+var whtieRabbit = {
+    type : "White",
+    "speak" : speak
+};
+
+whtieRabbit.speak("'Oh my ears and whiskers, how" + " late it's getting!");

8702e1ee0deed5f1a293a25fe48118bfc1797ae0
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-30 11:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter6/prototypes.js
+++ chapter6/prototypes.js
@@ -1,3 +1,24 @@
 console.log(Object.getPrototypeOf({}));
 
 console.log(Object.getPrototypeOf({}));
+console.log(Object.prototype);
+
+console.log(Object.getPrototypeOf([]) == Array.prototype);
+console.log(Array.prototype);
+console.log(Function.prototype);
+
+let protoRabbit = {
+    speak(line) {
+        console.log(`The ${this.type} rabbit says '${line}'`);
+    }
+};
+
+var killerRabbit = Object.create(protoRabbit);
+killerRabbit.type = "Killer";
+killerRabbit.speak("SKREEEE!");
+
+
+
+
+
+

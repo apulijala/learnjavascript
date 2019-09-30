1cebb0a1aa29a862c4ae01fd6cd080b2b818d8ce
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-09-30 17:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter6/Matrix.js
+++ chapter6/Matrix.js
@@ -1,13 +1,13 @@
 class Matrix {
+    
     constructor(width, height, element = (x,y) => undefined) {
-
-        width = this.width;
-        height = this.height;
+        this.width = width;
+        this.height = height;
         this.content = [];
 
         for (let y =0; y < height;y++) {
             for (let x =0; x < width; x++) {
-                content[y*width + x] = element(x,y); 
+                this.content[y*width + x] = element(x,y); 
             }
         }
     }
@@ -19,4 +19,42 @@ class Matrix {
     set(x,y, value) {
         this.content[y*this.width + x] = value;
     }
+}
+
+class MatrixIterator {
+    
+    constructor(matrix) {
+        this.x = 0;
+        this.y = 0;
+        this.matrix = matrix;
+    }
+
+    next() {
+
+        if (this.y == this.matrix.height) return {done : true};
+        let value = {
+                x : this.x,
+                y : this.y,
+                value : this.matrix.get(this.x, this.y)
+        };
+        this.x++;
+
+        if (this.x == this.matrix.width) {
+                this.x = 0;
+                this.y++;
+        }
+
+        return {value, done : false };
+    }
+
+}
+
+Matrix.prototype[Symbol.iterator] = function() {
+    return new MatrixIterator(this);
+}
+
+
+let matrix = new Matrix(2,2, (x,y) => `value ${x} ${y}`);
+for (let {x, y, value} of matrix) {
+    console.log(x, y, value);
 }

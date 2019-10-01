21249a4da604897a94519f194ebf25621c7b1b5d
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-01 17:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter5redo/repeat.js
+++ chapter5redo/repeat.js
@@ -29,6 +29,28 @@ let greaterThan10 = greaterThan(10);
 console.log("output of greater than");
 console.log(greaterThan10(11))
 
+console.log("output of greater than without intermediate variable");
+console.log(greaterThan(10)(11));
+
 console.log("Producing Units");
+let allunits = [];
+repeat(4, i => { 
+    allunits.push(`Unit ${i + 1}`);
+});
+console.log(allunits);
 
+function noisy(f) {
 
+    return (...args) => {
+        console.log("calling function with ", args);
+        let result = f(...args);
+        console.log("Called function with ", args , " and the result is " , result);
+        return result;
+    }
+}
+
+let result = noisy(Math.max)(3, 2, 1);
+console.log(result);
+
+result = noisy(Math.min)(3, 2, 1);
+console.log(result);

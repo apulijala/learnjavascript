16ecf92260a2d202111b88a0df580c8fa7c6aaea
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-01 19:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter5redo/reduction.js
+++ chapter5redo/reduction.js
@@ -1,13 +1,15 @@
 // takes an array, combine higher order function and 
 // a start element.
-function reduce (array, combine, start) {
+function reduce(array, combine, start) {
     let current = start;
-    for (let i of array) {
-        current = combine(current, i);
-    
+    for (let item of array) {
+        current = combine(current, item);
     }
     return current;
 }
 
+
 let result = reduce([1,2,3,4 ], (a,b) => a + b, 0);
 console.log(result);
+
+console.log([1,2,3,4].reduce((a,b) => a + b, 11));

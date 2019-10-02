1c4b6ab59ec7ce4e4f7282e7c42f5e70ee7a1248
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-02 13:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter6redo/beginning.js
+++ chapter6redo/beginning.js
@@ -19,9 +19,25 @@ let redRabbit = Object.create(rabbit);
 redRabbit.spaek();
 
 function speak(line) {
-    console.log(`The ${this.type} rabbit says ${line}`);
+    console.log(`The ${this.type} rabbit says '${line}'`);
 }   
 
+let whiteRabbbit = {
+    type : "White",
+    speak
+}
 
+let hungryRabbit = {
+    type : "Hungry",
+    speak
+}
+
+whiteRabbbit.speak("Oh my ears and whiskers, " + 
+    "how late it's getting!");
+hungryRabbit.speak("I could use a carrot right now.");
+function normalize() {
+    console.log(this.coords.map(k => k / this.length));
+}
+normalize.call({coords: [0, 2, 3], length : 5});
 
 

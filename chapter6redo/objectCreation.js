bf4259e359c014f3649eb5e0395b607ee8d995f2
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-02 12:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter6redo/objectCreation.js
+++ chapter6redo/objectCreation.js
@@ -19,6 +19,7 @@ protoRabbit.speak("Hello World!");
 whiteRabbit.speak("White Rabbit says ");
 whiteRabbit.sayHello("This word");
 
+
 class Rabbit {
     constructor(type) {
         this.type = type;
@@ -28,4 +29,29 @@ class Rabbit {
     }
 }
 let rabbit = new Rabbit("rabbit");
+rabbit.newProp = "value";
 rabbit.speak("hello");
+console.log(rabbit.newProp);
+
+/*
+function Rabbit(type) {
+    this.type = type;
+}
+
+Rabbit.prototype.speak = function(line) {
+    console.log(`The ${this.type} Rabbit says ${line}`);
+}
+
+let oneRabbit = new Rabbit("One");
+oneRabbit.speak("My World");
+
+console.log(Object.getPrototypeOf({}));
+console.log(Object.prototype);
+
+let myArray = [];
+console.log(Object.getPrototypeOf(myArray));
+console.log(Array.prototype);
+console.log(Object.getPrototypeOf(Rabbit));
+console.log(Function.prototype);
+*/
+

493144e6bc03470b6edf774a78faef04b85b8015
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-02 17:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter6redo/Matrix.js
+++ chapter6redo/Matrix.js
@@ -1,33 +1,32 @@
 class Matrix {
-    
-    constructor(width, height, element = (x,y) => undefined ) {
-        
+    constructor(width,height, element = (x,y) => undefined ) {
         this.width = width;
         this.height = height;
+        // Populate the matrix.
         this.content = [];
 
-        // Iniitate matrhix.
-        for (let y =0; y < this.height; y++) {
-            for (let x =0; x < this.width; x++) {
-              this.content[y*width + x] = element(x,y);
+        for (var k = 0; k < this.height; k++) {
+            for (var l =0; l < this.width; l++) {
+                this.content[k*this.width + l]  = element(l,k);
             }
         }
-       
     }
 
-    set(x,y) {
-        this.content[y*this.width + x] = element(x,y);
+    set (m,n, value) {
+        this.content[n*this.height + m] = value;
     }
 
-    get(x,y) {
-        return this.content[y*this.width + x];
+    get (o,p)  {
+        return this.content[o*this.height + p];
     }
-    
+
 }
 
 
-class MatrixIterator {
+let matrix = new Matrix(2,2, (x,y) => `${x} ${y}`);
+console.log(matrix);
 
+class MatrixIterator {
     constructor(matrix) {
         this.matrix = matrix;
         this.x = 0;
@@ -35,28 +34,29 @@ class MatrixIterator {
     }
 
     next() {
+
+        if (this.y == this.matrix.height) return {done : true}; 
         
-        if (this.y == this.matrix.height) return {done:true};
-        let value = {x : this.x, y: this.y , value : this.matrix.get(this.x,this.y)};
+        // extract the value.
+        let  value = {x : this.x, y : this.y, value : this.matrix.get(this.x, this.y)};
+
         this.x++;
-        if (this.x == this.matrix.width) {
-                this.y++;
-                this.x = 0;
+
+        if (this.x == this.width) {
+            this.y++;
+            this.x = 0;
         }
-        return {value : value, done : false}
+
+        return {value : value, done : false};
     }
+
+}
+
+Matrix.prototype[Symbol.iterator] =  function() {
+        return new MatrixIterator(this);
 }
 
 
-
-
-let matrix = new Matrix(2, 2, (x,y) => `value of ${x} ${y}`);
-Matrix.prototype[Symbol.iterator] = function() {
-    return new MatrixIterator(this);
+for (let {x, y, value} of matrix) {
+    console.log(`${x} ${y} ${value}` );
 }
-Matrix.prototype[Symbol.iterator]();
-for ({x,y, value} of matrix) {
-    console.log(x, y, value);
-}
-
-

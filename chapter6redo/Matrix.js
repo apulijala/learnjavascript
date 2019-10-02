e412c6b5c2d4702eab594faf3e043e8675c8e2f2
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-02 17:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter6redo/Matrix.js
+++ chapter6redo/Matrix.js
@@ -1,6 +1,7 @@
 class Matrix {
     
     constructor(width, height, element = (x,y) => undefined ) {
+        
         this.width = width;
         this.height = height;
         this.content = [];
@@ -8,10 +9,8 @@ class Matrix {
         // Iniitate matrhix.
         for (let y =0; y < this.height; y++) {
             for (let x =0; x < this.width; x++) {
-              // this.content.push(elemnt(x,y));
               this.content[y*width + x] = element(x,y);
             }
-
         }
        
     }
@@ -23,45 +22,41 @@ class Matrix {
     get(x,y) {
         return this.content[y*this.width + x];
     }
+    
 }
+
 
 class MatrixIterator {
 
-    constuructor(matrix) {
+    constructor(matrix) {
         this.matrix = matrix;
         this.x = 0;
         this.y = 0;
     }
 
     next() {
-        console.log("Before height");
-        console.log(this.matrix);
-        /*
+        
         if (this.y == this.matrix.height) return {done:true};
-        console.log("After height");
-        let value = {x : this.x, y: this.y , value : this.matrix.get(x,y)};
+        let value = {x : this.x, y: this.y , value : this.matrix.get(this.x,this.y)};
         this.x++;
         if (this.x == this.matrix.width) {
                 this.y++;
-                thix.x = 0;
+                this.x = 0;
         }
-
         return {value : value, done : false}
-        */
     }
 }
 
 
 
 
-let matrix = new Matrix(2, 2, (x,y) => `value of ${x} and ${y}`);
-// console.log(matrix);
-
+let matrix = new Matrix(2, 2, (x,y) => `value of ${x} ${y}`);
 Matrix.prototype[Symbol.iterator] = function() {
     return new MatrixIterator(this);
 }
-
-for (let {x, y, value} of matrix) {
+Matrix.prototype[Symbol.iterator]();
+for ({x,y, value} of matrix) {
     console.log(x, y, value);
-  }
+}
+
 

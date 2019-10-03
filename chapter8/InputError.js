18c91aba7125562ec6f92e5e06638508e853a18d
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-03 20:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter8/InputError.js
+++ chapter8/InputError.js
@@ -10,5 +10,22 @@ function promptDirection(direction) {
         return "L";
     }else if (direction.toLowerCase() == "r") {
         return "R";
-    }   
+    } else {
+        throw new InputError("Invalid Direction");
+    }  
+}
+
+try {
+
+    console.log("You chose " + promptDirection("l"));
+    console.log("You chose " + promptDirection("R"));
+    console.log("You chose " + promptDirection("n"));
+
+}catch (error) {
+    
+    if (error instanceof InputError) {
+        console.log(error);
+    }else {
+        throw error;
+    }
 }

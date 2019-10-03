f9540e024cbd2369fc429b50ecc6d41486b8d0ce
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-03 16:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter8/numberToString.js
+++ chapter8/numberToString.js
@@ -1,4 +1,6 @@
+"use strict"
 function numberToString(n, base = 10) {
+
     let result = "", sign = "";
     if (n < 0) {
         n = -n;
@@ -6,12 +8,31 @@ function numberToString(n, base = 10) {
     }
 
     do {
-        result = String(n % base) + result;
-        // reduce n.
+        result = n % base + result;
         n = Math.floor(n/base);
 
-    }while  (n > 0);
+    }while(n > 0);
     return sign + result;
 }
 
-console.log(numberToString(21,2));
+function numberToStringRecursive(n, base = 10) {
+    
+    function helperRecursive(n, result) {
+        if (n > 0 ) {
+            return result;
+        }else {
+            return helperRecursive(Math.floor(n/10),n %10 + result);
+        }
+
+    }
+    return helperRecursive(n, "");
+}
+
+console.log(numberToString(13,10));
+console.log(numberToString(-13,10));
+
+// Recurisve functions.
+
+console.log(numberToStringRecursive(13,10));
+console.log(numberToStringRecursive(-13,10));
+

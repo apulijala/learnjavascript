696b6138b10b3dbd052f2008c38d0fd3f19a5d18
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-03 19:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter8/Account.js
+++ chapter8/Account.js
@@ -1,7 +1,71 @@
 "use strict";
 
-let account = {
+let accounts = {
     a : 100,
     b: 0,
     c: 20
 };
+function getAccount(acctName) {
+    if (accounts.hasOwnProperty(acctName)) {
+        return acctName;
+    }else {
+        throw new Error("No Such Account : " + acctName);
+    }
+}
+
+/*
+function transferTo(from, money, to) {
+
+    if (account[from] > money) {
+        try {
+            account[from] -= money;
+            to = getAccount(to); 
+            account[to] += money;
+    
+        }catch(error) {
+            console.log(error);
+            
+        }
+        finally {
+            console.log("In finally");
+            account[from] += money;
+                
+        }
+    }
+}
+*/
+
+function transferTo(from, amount, to) {
+    
+    if (accounts[from] < amount) return;
+    let progress = 0;
+    if (accounts[from] > amount) {
+        progress = 1;
+        accounts[from] -= amount;
+        try {
+            accounts[getAccount(to)] +=amount;
+            progress = 2;
+        }finally {
+            if (progress == 1) {
+                accounts[from] -=amount;  
+            }
+            console.log("In finally");
+        }
+    }
+}
+
+
+
+console.log("Before Transfer");
+console.log(accounts);
+transferTo('a', 50, 'c');
+console.log("After transfer");
+console.log(accounts);
+
+/* unsuccessful path.
+console.log("Before Transfer");
+console.log(account);
+transferTo('a', 50, 'd');
+console.log("After transfer");
+console.log(account);
+*/

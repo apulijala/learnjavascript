4b2210df8e0f22c39ef35581f755aef6c4964605
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-03 15:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter8/numberToString.js
+++ chapter8/numberToString.js
@@ -2,12 +2,16 @@ function numberToString(n, base = 10) {
     let result = "", sign = "";
     if (n < 0) {
         n = -n;
+        sign = "-";
     }
+
     do {
         result = String(n % base) + result;
-        n = Math.floor(n/ base);
-    } while(n > 0);
+        // reduce n.
+        n = Math.floor(n/base);
+
+    }while  (n > 0);
     return sign + result;
 }
 
-console.log(numberToString(13,10));
+console.log(numberToString(21,2));

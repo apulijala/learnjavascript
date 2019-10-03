5c24fb8d16b1f89b0e08da29d73047f58e38f2ad
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-03 17:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

---
+++ chapter8/Exceptions.js
@@ -0,0 +1,11 @@
+"use strict"
+function promptDirection(direction) {
+    if (direction.toLowerCase() == "L") {
+        return "L"
+    }
+
+}
+
+promptDirection("L");
+promptDirection("R");
+promptDirection("N");

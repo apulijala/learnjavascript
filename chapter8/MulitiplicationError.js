28859a29cefd9fa4b1da70a6964cc25daf0047dd
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-03 21:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

---
+++ chapter8/MulitiplicationError.js
@@ -0,0 +1,21 @@
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+
+function reliableMultiply(a, b) {
+    try {
+     return  primitiveMultiply(a,b);
+    } catch (error) {
+        if (error instanceof MultiplicatorUnitFailure) {
+            return primitiveMultiply(a,b);
+        }
+    }
+}
+
+console.log(reliableMultiply(8, 8));

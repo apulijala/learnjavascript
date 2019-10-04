5c34e0afd6eb22b7f0c311a1a4ddaeac6c83ceb9
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-04 08:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter8/MulitiplicationError.js
+++ chapter8/MulitiplicationError.js
@@ -4,18 +4,21 @@ function primitiveMultiply(a, b) {
   if (Math.random() < 0.2) {
     return a * b;
   } else {
-    throw new MultiplicatorUnitFailure("Klunk");
+      throw new MultiplicatorUnitFailure("Klunk");
   }
 }
 
 function reliableMultiply(a, b) {
+    
     try {
-     return  primitiveMultiply(a,b);
-    } catch (error) {
+        return primitiveMultiply(a,b);
+        
+    } catch(error) {
         if (error instanceof MultiplicatorUnitFailure) {
             return primitiveMultiply(a,b);
         }
     }
 }
+
 
 console.log(reliableMultiply(8, 8));

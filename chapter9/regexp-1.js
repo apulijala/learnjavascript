876173091a65c6824f87740e5271b399f790f468
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-04 12:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter9/regexp-1.js
+++ chapter9/regexp-1.js
@@ -14,8 +14,18 @@ console.log(dateRegex.test("30-jan-2003 15:20"));
 dateRegex = /\d\d-\d\d-\d\d\d\d \d\d:\d\d/;
 console.log("Testing date Regular expression without new operator");
 console.log(dateRegex.test("01-30-2003 15:20"));
-
 console.log(dateRegex.test("30-jan-2003 15:20"));
+
+console.log("Regex to bind to any number except 0 or 1");
+let numRegex = /[^01]/;
+console.log(numRegex.test("1100100010200110"));
+console.log(numRegex.test("1100100010100110"));
+numRegex = new RegExp("[^01]");
+console.log("Regex to bind to any number except 0 or 1 with new operator");
+console.log(numRegex.test("1100100010200110"));
+console.log(numRegex.test("1100100010100110"));
+console.log("digit number");
+console.log(/'\d+'/.test("'123'"));
 
 
 
---
+++ chapter9/regexp-2.js
@@ -0,0 +1,4 @@
+// Testing regular expressions.
+
+console.log(/'\d+'/.test("'123'"));
+console.log(/'\d+'/.test("''"));

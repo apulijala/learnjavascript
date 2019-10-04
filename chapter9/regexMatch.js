e3004cf841f934823ececafcfaa7812a57f5dbea
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-04 19:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

---
+++ chapter9/regexMatch.js
@@ -0,0 +1,6 @@
+console.log("Barobudur".replace(/[ou]/g, "a"));
+
+let myString = "Liskov, Barbara\nMcCarthy, John\nWadler, Philip";
+console.log(myString.replace(/(\w+), (\w+)/, "$2, $1"));""
+
+let s = "the cia and fbi";

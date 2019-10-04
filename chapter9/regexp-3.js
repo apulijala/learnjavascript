e60d51431cdfc0db6d4be3748b351138f12685c7
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-04 16:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter9/regexp-3.js
+++ chapter9/regexp-3.js
@@ -4,3 +4,20 @@ let match = regex.exec(string);
 
 console.log("Match starts at " + match.index);
 console.log("String that matched " + match);
+
+console.log("In string");
+console.log("one two 100".match(/\d+/));
+
+var caseInsensitiveGroup = /boo+(hoo+)(hoo+)+/i;
+console.log(caseInsensitiveGroup.exec("Boohoooohoohooo"));
+
+let quotedText = /'([^']*)'/;
+console.log(quotedText.exec("she said 'hello'"));
+
+
+
+
+
+
+
+

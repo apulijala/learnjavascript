75782b60565af1d5c645fc9a5639e1e4370de7c5
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-04 18:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter9/dates.js
+++ chapter9/dates.js
@@ -9,11 +9,19 @@ console.log(new Date(1387411200000));
 function getDateFromString(dateString) {
     let dateRegex = /(\d{1,2})-(\d{1,2})-(\d{1,4})/;
     [_, month, day, year] = dateRegex.exec(dateString);
-    console.log(month);
-    console.log(day);
-    console.log(year);
-    
+    return new Date(year, month-1, day);
 }
-
 console.log("get date from string");
 console.log( getDateFromString("1-30-2003"));
+let regexWordBoundry = /\bcat\b/;
+console.log(regexWordBoundry.exec(" cat "));
+
+let animalregex = /\b\d+ (cow|pig|chicken)s?\b/;
+console.log(animalregex.test("15 chickens"));
+console.log(animalregex.test("1 cow"));
+
+
+
+
+
+

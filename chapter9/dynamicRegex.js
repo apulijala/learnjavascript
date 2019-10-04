fe034b2e868c83b1a5af10cf1934c89ac8c2a3ed
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-04 21:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter9/dynamicRegex.js
+++ chapter9/dynamicRegex.js
@@ -1,7 +1,33 @@
+/*
 let name = "harry";
 let s = "Harry is a suspicious character";
-let dynamicRegex = new RegExp( "\\b(" + name + ")\\b"  , "gi");
+let dynamicRegex = new RegExp("\\b(" + name + ")\\b"  , "gi");
 console.log(s.replace(dynamicRegex, "__$1__"));
 
-let name = "dea+hl[]rd";
+name = "dea+hl[]rd";
 // First replace name with escaping special characters.
+name = name.replace(/[+\\[]]/g, "\\$&");
+s = "This dea+hl[]rd guy is super annoying.";
+
+dynamicRegex = new RegExp("\\b(" + name + ")\\b", "gi");
+s.replace(dynamicRegex, "__$&__");
+
+console.log(" word".search(/\S/));
+
+let pattern = /y/g;
+pattern.lastIndex = 3;
+let match = pattern.exec("xyzzy");
+console.log(match.index);
+
+*/
+
+let global = /abc/g;
+global.lastIndex = 4;
+console.log(global.exec("xyz abc"));
+// → ["abc"]
+let sticky = /abc/y;
+console.log(sticky.exec("xyz abc"));
+// → null
+
+console.log("Banana".match(/an/g));
+

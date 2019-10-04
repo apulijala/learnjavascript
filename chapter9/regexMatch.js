956dfa4e7c33b0a802563e696de496b69e2ebc76
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-04 20:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter9/regexMatch.js
+++ chapter9/regexMatch.js
@@ -1,6 +1,31 @@
+/*
 console.log("Barobudur".replace(/[ou]/g, "a"));
 
 let myString = "Liskov, Barbara\nMcCarthy, John\nWadler, Philip";
 console.log(myString.replace(/(\w+), (\w+)/, "$2, $1"));""
 
 let s = "the cia and fbi";
+s.replace(/\bcia|fbi\b/g, str => console.log(str.toUpperCase()));
+
+*/
+
+let stock = "1 lemon, 2 cabbages, and 101 eggs.";
+function minusOne(match, amount, units) {
+    /*
+    console.log(match);
+    console.log(amount);
+    console.log(units);
+    */
+
+    amount = Number(amount) -1;
+    if (amount == 0) {
+        amount = "No";
+    }else if (amount == 1) {
+        units = String(units).substring(0, String(units).length -1);
+    }
+    return amount + " " + units;
+}
+
+console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
+
+

3723033071c37d583dcb50b37b4f4de3158c0659
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-04 12:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter9/regexp-1.js
+++ chapter9/regexp-1.js
@@ -4,3 +4,20 @@ console.log(eighteenPlus.test("ninehteennn"));
 
 let eightPlusRegex = new RegExp("eighteen\+");
 console.log(eightPlusRegex.test("eighteen+"));
+console.log(new RegExp("[0-9]").test("in 1992"));
+console.log(new RegExp("[0-9]").test("should return false"));
+
+var dateRegex = new RegExp("\\d\\d-\\d\\d-\\d\\d\\d\\d \\d\\d:\\d\\d");
+console.log("Testing date Regular expression");
+console.log(dateRegex.test("01-30-2003 15:20"));
+console.log(dateRegex.test("30-jan-2003 15:20"));
+dateRegex = /\d\d-\d\d-\d\d\d\d \d\d:\d\d/;
+console.log("Testing date Regular expression without new operator");
+console.log(dateRegex.test("01-30-2003 15:20"));
+
+console.log(dateRegex.test("30-jan-2003 15:20"));
+
+
+
+
+

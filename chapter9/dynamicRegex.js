dfce9fc67e02f2dfd7be5e33188290110f6704a9
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-05 14:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter9/dynamicRegex.js
+++ chapter9/dynamicRegex.js
@@ -14,3 +14,19 @@ console.log("she said 'hello' and is 'hello'".replace(quotedText, "replaced"));
 
 quotedText = /xyz/;
 quotedText.exec("This is xyz and xyz");
+
+let digit = /\d/g;
+console.log(digit.exec("here it is: 1"));
+console.log(digit.exec("here it is: 1"));
+
+console.log(")");
+let bregEx = /an/g;
+console.log("Banana".match(bregEx));
+
+let myStr = "A string with 3 numbers in it... 42 and 88.";;
+let match1;
+let digitRegex = new RegExp("\\d+", "g");
+
+while (match1 = digitRegex.exec(myStr)) {
+    console.log("Found " + match1[0] + " at index " + match1.index);
+}

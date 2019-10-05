b7464e5a2e2932be7e20158c9e363d1bc3845e80
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-05 19:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter9redo/ch6-parseIni.js
+++ chapter9redo/ch6-parseIni.js
@@ -3,19 +3,21 @@ function parseINI(text) {
     let result ={};
     let content = result;
 
-    String(text).split("\r?\n").forEach(
+    String(text).split(/\r?\n/).forEach(    
+    
     (line) => {
-        
-        if (line.match(/\w+=/)) {
-             console.log(line);
-        }
+
+        let match;
+        if (match = line.match(/^(\w+)=(.*)$/)) {
+            content[match[1]] = match[2];
+          }else if (match = line.match(/\[(.*)\]/)) {
+           // content.log(match[1]);
+          }
 
     });
 
     return result;
 }
-
-
 
 console.log(parseINI(`
 name=Vasilis

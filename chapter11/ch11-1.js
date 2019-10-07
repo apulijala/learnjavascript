95eb8980bfca6266062d75db991e000e498ae54c
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-07 16:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter11/ch11-1.js
+++ chapter11/ch11-1.js
@@ -1,1 +1,9 @@
-setTimeout(() => {console.log("Tick")}, 15000);
+// setTimeout(() => {console.log("Tick")}, 15000);
+let {bigOak} = require("./crow-tech");
+bigOak.readStorage("food caches",
+        caches => {
+            let firstCache = caches[0];           
+            bigOak.readStorage(firstCache, info => {
+                console.log(info);
+            });
+        });

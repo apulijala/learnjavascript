c0fa49ddf7d41f78bb43c740ac36d0f0142bb976
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-07 13:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter10/ch10-ex3.js
+++ chapter10/ch10-ex3.js
@@ -1,11 +1,15 @@
 const {formatDate,helloWorld} = require("./format-date.js");
 
-
+/*
 console.log(formatDate(new Date(2019, 8, 13),
                        "dddd the Do"));
+*/
+
+console.log(formatDate(new Date(2019, 8, 13),
+                       "dddd the Do YYYY"));                       
 
 // Should print  Friday the 13th                   
 
-console.log(helloWorld("Andrew"));
+// console.log(helloWorld("Andrew"));
 
 // should print Hello World to Andrew
--- chapter10/format-date.js
+++ chapter10/format-date.js
@@ -1,7 +1,24 @@
 const ordinal = require("ordinal");
-const {days} = require("date-names");
+const {days, months} = require("date-names");
 
+/*
 console.log(days[1]);
-//console.log(months[4]);
+console.log(months[4]);
 console.log(ordinal(25));
+console.log(/YYYY/.test("YYYY"));
+*/
 
+
+exports.formatDate = function(date, format) {
+    return String(format).replace(/YYYY|M(MMM)?|D(o)?|dddd/g, str => {
+
+        if (str == "YYYY") return date.getFullYear();
+        if (str == "M") return date.getMonth();
+        if (str == "MMMM") return ordinal(months[date.getMonth()]);
+        if (str == "D") return  date.getDate();
+        if (str == "Do") return  ordinal(date.getDate());
+        if (str == "dddd") return  days[date.getDay()]; 
+
+    });
+
+}

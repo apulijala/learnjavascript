ebf92f7c155efc6174cce36e93cd14bd7899dabf
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-07 17:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter11/ch11-1.js
+++ chapter11/ch11-1.js
@@ -1,5 +1,5 @@
 // setTimeout(() => {console.log("Tick")}, 15000);
-let {bigOak} = require("./crow-tech");
+let {bigOak, defineRequestType} = require("./crow-tech");
 bigOak.readStorage("food caches",
         caches => {
             let firstCache = caches[0];           
@@ -7,3 +7,36 @@ bigOak.readStorage("food caches",
                 console.log(info);
             });
         });
+
+bigOak.send("Cow Pasture", "note", "Let's caw loudly at 7PM", 
+        () => {
+            console.log("Note Delivered");
+        });
+
+defineRequestType("note", (nest, content, source, done) => {
+    console.log(`${nest.name} received note: ${content}`);
+    done();
+});
+
+// Promises.
+let fifteen = Promise.resolve(15);
+fifteen.then(
+    value => console.log(`Got ${value}`)
+);
+
+function storage(nest, name) {
+    return new Promise(resolve => {
+        nest.readStorage(name => result => {
+            
+        });
+}
+
+
+
+
+
+
+
+
+
+

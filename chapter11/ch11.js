152848c130257a18884abea3ec7f21cdef793a1b
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-08 11:30:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter11/ch11.js
+++ chapter11/ch11.js
@@ -1,6 +1,11 @@
 const {bigOak, defineRequestType} = require("./crow-tech");
 bigOak.readStorage("food caches", (caches) => {
-
+    console.log("Top Level Caches");
     console.log(caches);
+    caches.forEach(element => {
+        console.log("cache of " + element);
+        bigOak.readStorage(element, caches);
+        console.log(caches);
+    });
 
 });

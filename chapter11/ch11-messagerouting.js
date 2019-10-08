50890e213fa7208184dc56a8e0e136ae8f5505be
Arvind Pulijala <arvind_pulijala@hotmail.com>
2019-10-08 15:00:01 +0100

    Timed backup
    Hi from the template commit-msg hook

--- chapter11/ch11-messagerouting.js
+++ chapter11/ch11-messagerouting.js
@@ -1,5 +1,23 @@
 const {bigOak, defineRequestType, everywhere} = require("./crow-tech");
 
+function request(nest, target, type, content) {
+    return new Promise((resolve, reject) => {
+      let done = false;
+      function attempt(n) {
+        nest.send(target, type, content, (failed, value) => {
+          done = true;
+          if (failed) reject(failed);
+          else {   resolve(value)};
+        });
+        setTimeout(() => {
+          if (done) return;
+          else if (n < 3) attempt(n + 1);
+          else reject(new Timeout("Timed out"));
+        }, 250);
+      }
+      attempt(1);
+    });
+  }
 
 function requestType(name, handler) {
     defineRequestType(name, (nest, content, source,
@@ -15,13 +33,40 @@ function requestType(name, handler) {
   }
 
   // Gets triggered when message is sent.
-  
-requestType("gossip", (nest, message, source) => {
-    if (nest.state.gossip.includes(message)) return;
-    console.log(`${nest.name} received gossip '${
-                 message}' from ${source}`);
-    sendGossip(nest, message, source);
-  }) ;
+
+
+
+requestType("connections", (nest, {name, neighbors},
+    source) => {
+let connections = nest.state.connections;
+        
+    if (JSON.stringify(connections.get(name)) ==
+
+        JSON.stringify(neighbors)) return;
+        connections.set(name, neighbors);
+        broadcastConnections(nest, name, source);
+});
+
+function broadcastConnections(nest, name, exceptFor = null) {
+    
+    for (let neighbor of nest.neighbors) {
+      if (neighbor == exceptFor) continue;
+          request(nest, neighbor, "connections", {
+            name,
+        neighbors: nest.state.connections.get(name)
+      });
+    }
+  }
+
+  everywhere(nest => {
+    nest.state.connections = new Map;
+    nest.state.connections.set(nest.name, nest.neighbors);
+    broadcastConnections(nest, nest.name);
+    console.log(nest.state.connections);
+ });
+
+
+
 
 
   
--- chapter11/ch11-resolve.js
+++ chapter11/ch11-resolve.js
@@ -85,7 +85,29 @@ function sendGossip(nest, message, exceptFor = null) {
     }
   }
 
-  sendGossip(bigOak, "Digambara Digambara Sripada Vallabha Digambara")
+  sendGossip(bigOak, "Digambara Digambara Sripada Vallabha Digambara");
+
+
+  requestType("connections", (nest, {name, neighbors},
+    source) => {
+let connections = nest.state.connections;
+if (JSON.stringify(connections.get(name)) ==
+
+
+JSON.stringify(neighbors)) return;
+connections.set(name, neighbors);
+broadcastConnections(nest, name, source);
+});
+
+function broadcastConnections(nest, name, exceptFor = null) {
+for (let neighbor of nest.neighbors) {
+if (neighbor == exceptFor) continue;
+request(nest, neighbor, "connections", {
+name,
+neighbors: nest.state.connections.get(name)
+});
+}
+}
 
 
 
